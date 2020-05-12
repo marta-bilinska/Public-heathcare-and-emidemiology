d02f2fdec7385527d7b2874a36a6ea266efe7d3b
Marta <marta@intteh.com>
2020-05-12 11:45:22 +0300

    total value experiment added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 !!! Notice that for this program to be faster, the file that is used in it
 is an excerpt from the whole file.
 """
-
+import numpy
 import pandas as pd
 from data_structures import DiseaseTree, Node, ListNode, Disease
 
@@ -24,9 +24,12 @@ def read_into_data_structure():
         year = int(diseases_data.values[2])
         place = str(diseases_data.values[6])
         sex = str(diseases_data.values[3])
-        value = float(diseases_data.values[5])
+        value = diseases_data.values[5]
+        if numpy.isnan(value):
+            value = float(0)
+        else:
+            value = float(value)
         ethnicity = str(diseases_data.values[4])
-
         if indicator not in diseases_tree.children:
             diseases_tree.add_child(indicator, Node('indicator', diseases_tree))
         indicator_node = diseases_tree.children[indicator]
@@ -54,9 +57,30 @@ def experiment_cities(data, indicator):
     return cities
 
 
+def total_value_experiment(data, indicator):
+    """
+    DiseaseTree, str -> str
+    Does experiments with values of all diseases.
+    """
+    disease_node = data.children[indicator]
+    value = 0
+    for i in disease_node.children:
+        disease = disease_node.children[i]
+        for i in disease.children:
+            value += float(i.value)
+    return value
+
+
 def main():
-        disease_data = read_into_data_structure()
-        print(experiment_cities(disease_data, 'Behavioral Health/Substance Abuse'))
+    """
+    Main function that conducts experiments.
+    """
+    disease_data = read_into_data_structure()
+    city_experiment = experiment_cities(disease_data, 'Behavioral Health/Substance Abuse')
+    print(city_experiment)
+    value_experiment = total_value_experiment(disease_data, 'Behavioral Health/Substance Abuse')
+    string_value = str(round(value_experiment, 2))
+    print("The total value of the cities is: ", string_value, "Per 100 000 of population")
 
 
 if __name__ == "__main__":
